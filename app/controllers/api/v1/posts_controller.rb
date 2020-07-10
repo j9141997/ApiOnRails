89281cd0542b91ed1b4324cc637947599024f7ce
Junki Yoshida <j9141997@gmail.com>
2020-07-10 22:18:12 +0900

    Init Commit

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      before_action :set_post, only: [:show, :create, :update]
+      
+      def index
+        
+      end
+
+      def show
+
+      end
+
+      def new
+
+      end
+
+      def create
+
+      end
+
+      def update
+
+      end
+    end
+  end
+end
