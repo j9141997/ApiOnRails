0a5ae263a2173af83b86bfe39343f4bc29d21368
junki-yoshida <junkiyoshida@play.jp>
2020-07-20 23:49:10 +0900

    change setting database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_10_130046) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
